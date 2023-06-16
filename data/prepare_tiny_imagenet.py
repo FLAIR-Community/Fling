edf862221c0480771b695bf60e6eb8ac9d584bd5
‘whl’ <18231213@buaa.edu.cn>
2023-06-16 18:22:13 +0800

    polish(whl): manual add tracked files.

---
+++ data/prepare_tiny_imagenet.py
@@ -0,0 +1,32 @@
+import os
+from shutil import move
+from os import rmdir
+
+if __name__ == '__main__':
+    os.system('wget http://cs231n.stanford.edu/tiny-imagenet-200.zip')
+    os.system('unzip tiny-imagenet-200.zip')
+    target_folder = './tiny-imagenet-200/val/'
+
+    val_dict = {}
+    with open('./tiny-imagenet-200/val/val_annotations.txt', 'r') as f:
+        for line in f.readlines():
+            split_line = line.split('\t')
+            val_dict[split_line[0]] = split_line[1]
+
+    paths = os.listdir('./tiny-imagenet-200/val/images')
+    for path in paths:
+        path = './tiny-imagenet-200/val/images/' + path
+        file = path.split('/')[-1]
+        folder = val_dict[file]
+        if not os.path.exists(target_folder + str(folder)):
+            os.mkdir(target_folder + str(folder))
+            os.mkdir(target_folder + str(folder) + '/images')
+
+    for path in paths:
+        path = './tiny-imagenet-200/val/images/' + path
+        file = path.split('/')[-1]
+        folder = val_dict[file]
+        dest = target_folder + str(folder) + '/images/' + str(file)
+        move(path, dest)
+
+    rmdir('./tiny-imagenet-200/val/images')
