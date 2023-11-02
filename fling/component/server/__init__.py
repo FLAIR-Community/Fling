7118fda5ff28efbaa43bc2f3a2ffff76ec76d041
Kye <1045552804@qq.com>
2023-11-02 15:32:32 +0800

    add FedMOON demo

--- fling/component/server/__init__.py
+++ fling/component/server/__init__.py
@@ -1,3 +1,4 @@
 from .server_template import ServerTemplate
 from .base_server import BaseServer
 from .build_server import get_server
+from .fedmoon_server import FedMOONServer
--- fling/model/__init__.py
+++ fling/model/__init__.py
@@ -5,6 +5,7 @@ from .cnn import CNNModel
 from .resnet import resnet4, resnet6, resnet8, resnet10, resnet18, resnet34, resnet50
 from .swin_transformer import SwinTransformer
 from .vit import ViT
+from .moon_models import FedMOONModel
 from .build_model import get_model
 
 # Algorithm specific models
