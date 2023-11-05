d39458e0dcf43ff013c6fc589e909cea2885d264
Wang hl <59834623+kxzxvbk@users.noreply.github.com>
2023-11-05 22:18:19 +0800

    feature(whl): add mini-imagenet dataset. (#27)
    * init commit
    
    * update
    
    * polish data aug
    
    ---------
    
    Co-authored-by: ‘whl’ <‘18231213@buaa.edu.cn’>

---
+++ argzoo/mini_imagenet/mini_imagenet_fedavg_resnet_config.py
@@ -0,0 +1,28 @@
+from easydict import EasyDict
+
+exp_args = dict(
+    data=dict(
+        dataset='mini_imagenet',
+        data_path='./data/mini-imagenet',
+        sample_method=dict(name='iid', train_num=500, test_num=100)
+    ),
+    learn=dict(
+        device='cuda:0', local_eps=8, global_eps=40, batch_size=32, optimizer=dict(name='sgd', lr=0.02, momentum=0.9)
+    ),
+    model=dict(
+        name='resnet8',
+        input_channel=3,
+        class_number=100,
+    ),
+    client=dict(name='base_client', client_num=40),
+    server=dict(name='base_server'),
+    group=dict(name='base_group', aggregation_method='avg'),
+    other=dict(test_freq=3, logging_path='./logging/mini_imagenet_fedavg_resnet_iid')
+)
+
+exp_args = EasyDict(exp_args)
+
+if __name__ == '__main__':
+    from fling.pipeline import generic_model_pipeline
+
+    generic_model_pipeline(exp_args, seed=0)
--- fling/dataset/__init__.py
+++ fling/dataset/__init__.py
@@ -2,4 +2,5 @@ from .cifar10 import CIFAR10Dataset
 from .cifar100 import CIFAR100Dataset
 from .mnist import MNISTDataset
 from .tiny_imagenet import TinyImagenetDataset
+from .mini_imagenet import MiniImagenetDataset
 from .build_dataset import get_dataset
