638a3a808e9232484ee7772a91466b028113a99e
‘whl’ <18231213@buaa.edu.cn>
2023-11-07 17:49:27 +0800

    init commit

--- fling/dataset/__init__.py
+++ fling/dataset/__init__.py
@@ -3,4 +3,5 @@ from .cifar100 import CIFAR100Dataset
 from .mnist import MNISTDataset
 from .tiny_imagenet import TinyImagenetDataset
 from .mini_imagenet import MiniImagenetDataset
+from .imagenet import ImagenetDataset
 from .build_dataset import get_dataset
