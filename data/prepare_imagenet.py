3a5e3be87773f4c86fd3c5d353ff2978efaaf86a
‘whl’ <18231213@buaa.edu.cn>
2023-11-10 11:03:53 +0800

    add file

---
+++ data/prepare_imagenet.py
@@ -0,0 +1,6 @@
+"""
+This file will convert the raw Imagenet dataset to lmdb files for faster data loading. \
+Assume that you have already downloaded the ImageNet dataset from https://image-net.org/, the files is organized as:
+.
+
+"""
