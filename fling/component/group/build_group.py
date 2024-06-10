ee3fe4c378450785e694a00ef368ee7c1e2a8946
Wang hl <59834623+kxzxvbk@users.noreply.github.com>
2024-06-10 14:19:52 +0800

    init commit (#44)
    Co-authored-by: ‘whl’ <‘18231213@buaa.edu.cn’>

--- fling/component/group/build_group.py
+++ fling/component/group/build_group.py
@@ -3,5 +3,5 @@ from fling.utils import Logger
 from fling.component.group import ParameterServerGroup
 
 
-def get_group(args: dict, logger: Logger) -> ParameterServerGroup:
-    return GROUP_REGISTRY.build(args.group.name, args, logger)
+def get_group(args: dict, logger: Logger, **kwargs) -> ParameterServerGroup:
+    return GROUP_REGISTRY.build(args.group.name, args, logger, **kwargs)
