b7e7e1270c48642d847bb5b6bb88aa1078291dc5
‘whl’ <18231213@buaa.edu.cn>
2024-06-10 14:18:00 +0800

    init commit

--- fling/dataset/build_dataset.py
+++ fling/dataset/build_dataset.py
@@ -3,5 +3,5 @@ from torch.utils.data.dataset import Dataset
 from fling.utils.registry_utils import DATASET_REGISTRY
 
 
-def get_dataset(args: dict, train: bool) -> Dataset:
-    return DATASET_REGISTRY.build(args.data.dataset, args, train)
+def get_dataset(args: dict, train: bool, **kwargs) -> Dataset:
+    return DATASET_REGISTRY.build(args.data.dataset, args, train, **kwargs)
