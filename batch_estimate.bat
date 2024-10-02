e3d400a71d62eaa0170c1f8dbc42bdfe6e383073
‘whl’ <18231213@buaa.edu.cn>
2024-10-02 12:40:02 +0800

    remove redundant files

--- batch_estimate.bat
+++
@@ -1,3 +0,0 @@
-python estimiate_k.py --model resnet18 --train-epo 0 --number 10000
-python estimiate_k.py --model resnet18 --train-epo 1 --number 10000
-python estimiate_k.py --model resnet18 --train-epo 2 --number 10000
